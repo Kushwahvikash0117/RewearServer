842c8d076f2050d4fbf10b1dcd2674985778720c
Vikash Kushwah <vikash.vk.0117@gmail.com>
2025-10-15 00:30:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ import itemRoutes from './routes/itemRoutes.js';
 import swapRoutes from './routes/swapRoutes.js';
 import errorHandler from './middlewares/errorHandler.js';
 
-loadEnv();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
