9de46621e95c6d0bf380c1b1245f61daf9017ddc
Vikash Kushwah <vikash.vk.0117@gmail.com>
2025-11-15 15:58:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+    origin: ["https://rewear-alpha.vercel.app", "http://localhost:5173"], 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
