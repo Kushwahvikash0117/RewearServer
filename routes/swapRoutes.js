c7d1183768dcc54d1844e82bf2bbfd449a3150a1
Vikash Kushwah <vikash.vk.0117@gmail.com>
2025-11-15 11:48:33 +0530

    Update swapRoutes.js

--- routes/swapRoutes.js
+++ routes/swapRoutes.js
@@ -1,15 +1,11 @@
 import express from "express";
 import { createSwapRequest, respondToSwap, getAllSwaps } from "../controllers/swapController.js";
+import protect from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// Create a swap request
-router.post("/request", createSwapRequest);
-
-// Respond to a swap (accept / reject)
-router.post("/respond", respondToSwap);
-
-// View all swaps
-router.get("/", getAllSwaps);
+router.post("/request", protect, createSwapRequest);
+router.post("/respond", protect, respondToSwap);
+router.get("/", protect, getAllSwaps);
 
 export default router;
