3acb2803cfeea6ae07a70e32469e2e3a6e1803e7
Vikash Kushwah <vikash.vk.0117@gmail.com>
2026-06-10 16:56:46 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://vikashvk0117:NJ6hlHB56fzMDgw6@rewear.gkc73p8.mongodb.net/rewear");
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
